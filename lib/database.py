ee8afe9879cb0d8df89e46c0e9bc891d1fc91ef3
Micha Gorelick <mynameisfiber@gmail.com>
2014-06-01 17:33:09 -0600

    cleaned up cardview

--- lib/database.py
+++ lib/database.py
@@ -8,6 +8,8 @@ for table in os.listdir("./data"):
     database = {}
     table_path = os.path.join("./data", table)
     for item in os.listdir(table_path):
+        if not item.endswith(".json"):
+            continue
         item_id = int(item.split(".")[0]) #dirty but i'm tired
         item_path = os.path.join(table_path, item)
         database[item_id] = json.load(open(item_path))
--- static/js/cardview.js
+++ static/js/cardview.js
@@ -1,42 +1,47 @@
-function cardview_init() {
-    var cardcontainers = $('.card_user_node_root');
-    _.each(cardcontainers, _get_card_info);
-}
+var CardView = (function(jQuery, _) {
+    var module = {};
 
-function _get_card_info(card) {
-    var card_jq = $(card);
-    var node_id = card_jq.attr('nodeid');
-    var api_url = '/api/v0/getusernodegraph?node_id=' + node_id;
-    $.getJSON(api_url, function(data) {
-        _fill_card(card_jq, data);
-    }).fail(function() {
-        card_jq.html("<p class='card_error'>Could not find content</p>");
-    });
-}
+    var user_node_template = _.template("<%= title %>");
+    var content_node_template = _.template("<span class='title'><%= title %></span><span class=description><%= description %></span>");
 
-var user_node_template = _.template("<%= title %>");
-var content_node_template = _.template("<span class='title'><%= title %></span><span class=description><%= description %></span>");
-
-function _fill_card(htmlnode, root) {
-    var content_div = $("<p/>").addClass("card_content");
-    var root_div = $("<div/>");
-    content_div.appendTo(root_div);
-
-    if (root.type == 'content_node') {
-        root_div.addClass('card_content_node')
-        $.getJSON('/api/v0/getcontentnode?node_id=' + root.id, function(data) {
-            content_div.html(content_node_template(data));
+    module.init = function() {
+        var cardcontainers = jQuery('.card_user_node_root');
+        _.each(cardcontainers, module.init_card);
+    }
+    
+    module.init_card = function(card) {
+        var card_jq = jQuery(card);
+        var node_id = card_jq.attr('nodeid');
+        var api_url = '/api/v0/getusernodegraph?node_id=' + node_id;
+        jQuery.getJSON(api_url, function(data) {
+            _fill_card(card_jq, data);
         }).fail(function() {
+            card_jq.html("<p class='card_error'>Could not find content</p>");
+        });
+    }
+    
+    function _fill_card(htmlnode, root) {
+        var content_div = jQuery("<p/>").addClass("card_content");
+        var root_div = jQuery("<div/>");
+        content_div.appendTo(root_div);
+    
+        if (root.type == 'content_node') {
+            root_div.addClass('card_content_node')
             root.description = "";
             content_div.html(content_node_template(root));
-        });
-    } else {
-        content_div.html(user_node_template(root));
-        root_div.addClass('card_user_node');
-        root_div.addClass('cf');
+            jQuery.getJSON('/api/v0/getcontentnode?node_id=' + root.id, function(data) {
+                content_div.html(content_node_template(data));
+            })
+        } else {
+            content_div.html(user_node_template(root));
+            root_div.addClass('card_user_node');
+            root_div.addClass('cf');
+        }
+        root_div.appendTo(htmlnode);
+        
+        var fill_child = _.partial(_fill_card, root_div);
+        _.each(root.children, fill_child);
     }
-    root_div.appendTo(htmlnode);
-    
-    var fill_child = _.partial(_fill_card, root_div);
-    _.each(root.children, fill_child);
-}
+
+    return module;
+})(jQuery, _)
