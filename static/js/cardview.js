7b3d9a88ba8c4fa71f48c1a9f88c0ef4869a4e40
Micha Gorelick <mynameisfiber@gmail.com>
2014-06-01 16:26:28 -0600

    finish v1 of cardview

--- static/js/cardview.js
+++ static/js/cardview.js
@@ -1,9 +1,9 @@
 function cardview_init() {
     var cardcontainers = $('.card_user_node_root');
-    cardcontainers.each(_get_card_info);
+    _.each(cardcontainers, _get_card_info);
 }
 
-function _get_card_info(i, card) {
+function _get_card_info(card) {
     var card_jq = $(card);
     var node_id = card_jq.attr('nodeid');
     var api_url = '/api/v0/getusernodegraph?node_id=' + node_id;
@@ -12,19 +12,26 @@ function _get_card_info(i, card) {
     });
 }
 
+var user_node_template = _.template("<%= title %>");
+var content_node_template = _.template("<span class='title'><%= title %></span><span class=description><%= description %></span>");
+
 function _fill_card(htmlnode, root) {
-    var root_div = $("<div/>").html("Node: " + root.title);
+    var content_div = $("<p/>").addClass("card_content");
+    var root_div = $("<div/>");
+    content_div.appendTo(root_div);
+
     if (root.type == 'content_node') {
         root_div.addClass('card_content_node')
         $.getJSON('/api/v0/getcontentnode?node_id=' + root.id, function(data) {
-            root_div.html('Content: ' + data.title + ': ' + data.description);
+            content_div.html(content_node_template(data));
         });
     } else {
+        content_div.html(user_node_template(root));
         root_div.addClass('card_user_node');
+        root_div.addClass('cf');
     }
     root_div.appendTo(htmlnode);
     
-    for (var i=0; i<root.children.length; i++) {
-        _fill_card(root_div, root.children[i]);
-    }
+    var fill_child = _.partial(_fill_card, root_div);
+    _.each(root.children, fill_child);
 }
