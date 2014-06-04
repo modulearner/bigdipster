9a07abf5d6bed645448fff0ef02c4f748f028dbb
Micha Gorelick <mynameisfiber@gmail.com>
2014-06-04 02:55:25 -0600

    working on textbook view

--- static/js/textbookview.js
+++ static/js/textbookview.js
@@ -1,4 +1,4 @@
-var TextbookView = (function(jQuery, d3, _) {
+var TextbookView = (function(jQuery, d3, markdown, _) {
     var module = {};
 
     module.init = function() {
@@ -35,8 +35,10 @@ var TextbookView = (function(jQuery, d3, _) {
         for (var i in data) {
             var j = parseInt(i);
             nodes.push({
-                "name" : data[i].title,
+                "title" : data[i].title,
+                "uid" : _uid(data[i]),
                 "id" : j,
+                "y" : j/(data.length-1),
             });
             links.push({
                 "source" : j,
@@ -44,54 +46,88 @@ var TextbookView = (function(jQuery, d3, _) {
             })
         }
         links.pop();
-        console.log(links);
         return {nodes: nodes, links: links}
+    }
+
+    function _uid(d,i) {
+        return "section_" + d.title.replace(" ", '') + i;
     }
 
     function _fill_graph(div, data) {
         var graph = _create_graph(_flatten_graph(data));
         
+        var width = 200, 
+            height=$(window).height();
+
         var svg = d3.selectAll(div).append("svg")
             .attr("width", "100%")
             .attr("height", "100%");
 
-        
-          var node = svg.selectAll(".node")
-              .data(graph.nodes)
-            .enter().append("circle")
-              .attr("class", "node")
-              .attr("r", 25)
-              .attr("cx", 100)
-              .attr("cy", function(d,i) { return i*100; });
+        var node = svg.selectAll(".node")
+            .data(graph.nodes)
+          .enter().append("circle")
+            .attr("class", function(d,i) { return "node " + _uid(d,i); })
+            .attr("r", 25)
+            .attr("cx", width/2)
+            .attr("cy", function(d,i) { return height * d.y; });
 
-          var link = svg.selectAll(".link")
-              .data(graph.links)
-            .enter().append("line")
-              .attr("class", "link")
-              .attr("x1", 100)
-              .attr("y1", function(d) { return d.source*100 + 25; })
-              .attr("x2", 100)
-              .attr("y2", function(d) { return d.target*100 - 25; });
+        jQuery(document).scroll(function(event) { _magnify_graph(div, event) });
+
+        // TODO: figure out a good way of doing links... do i need force directed
+        // in order to get the `d.source.x` properties?
+        //var link = svg.selectAll(".link")
+        //    .data(graph.links)
+        //  .enter().append("line")
+        //    .attr("class", "link")
+        //    .attr("x1", 100)
+        //    .attr("y1", function(d) { return d.source*100 + 25; })
+        //    .attr("x2", 100)
+        //    .attr("y2", function(d) { return d.target*100 - 25; });
         
-          node.append("title")
-              .text(function(d) { return d.name; });
+        node.append("title")
+            .text(function(d) { return d.title; });
     }
 
-    function _fill_text(div, data, level) {
+    function _magnify_graph(div, event) {
+        foo = event;
+        var factor = $(document).scrollTop() / $(document).height();
+        d3.selectAll(div).selectAll(".node")
+            .attr("r", function(d,i) { 
+                return 25 / (8 * Math.pow(factor - d.y, 2) + 1);
+            });
+    
+    }
+
+    function _fill_text(div, data, level, num_children) {
         var level = level || 1;
+        var num_children = num_children || 0;
         jQuery("<h" + level + "/>").html(data.title).appendTo(div);
 
         if (data.type == 'content_node') {
-            var content = jQuery("<div/>").appendTo(div);
-            jQuery.getJSON('/api/v0/getcontentnode?node_id=' + data.id, function(data) {
-                content.html(data.text);
+            var content = jQuery("<div/>").addClass("markdown").addClass(_uid(data, num_children)).appendTo(div);
+            jQuery.getJSON('/api/v0/getcontentnode?node_id=' + data.id, function(data) { 
+                var mdtree = _reformat_markdown_headings(markdown.parse(data.text), level);
+                //var html = markdown.renderJsonML(markdown.toHTMLTree(mdtree));
+                var html = markdown.toHTML(mdtree);
+                content.html(html);
             })
+            return true;
         }
 
         for (var i in data.children) {
-            _fill_text(div, data.children[i], level+1);
+            var is_child = _fill_text(div, data.children[i], level+1, num_children);
+            num_children += is_child;
         }
     }
 
+    function _reformat_markdown_headings(mdtree, level) {
+        _.each(mdtree, function(d,i) {
+            if (i != 0) {
+                d[1].level += level;
+            }
+        })
+        return mdtree;
+    }
+
     return module;
-})(jQuery, d3, _)
+})(jQuery, d3, markdown, _)
